231bba285cc293a15b6bedb4efb0e9d58832d2b7
AvadEu <dutwik44@wp.pl>
2023-03-09 19:46:59 +0100

    Create func to read config file
    Imported databse models module, os.path for proper config file path, tomlib for reading config. Created function that reads toml config file and returns it as a dict. Establish models.init_all() and uvicorn.run for delivered config file settings.

--- API/app.py
+++ API/app.py
@@ -1,7 +1,18 @@
+import models
 import uvicorn
+import tomllib
+from os import path
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# Function that reads application config and return it as a dictionary
+def read_conf() -> dict:
+    conf_path = path.join(path.dirname(path.dirname(path.abspath(__file__))),'conf.toml')
+    with open(conf_path, 'rb') as f:
+        conf = tomllib.load(f)
+        return conf
+
 
 @app.get('/')
 def hello_world():
@@ -9,4 +20,6 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    uvicorn.run('app:app', host='127.0.0.1', port=4040)
+    api_conf, db_conf = list(read_conf().values()[:2])
+    engine = models.init_all(db_conf)
+    uvicorn.run('app:app', host=api_conf['host'], port=api_conf['port'], reload=api_conf['reload'])
