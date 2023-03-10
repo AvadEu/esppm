4b418de763c8972019ccb96ca13002e767eee8e7
AvadEu <dutwik44@wp.pl>
2023-03-10 10:56:43 +0100

    Modify read_conf() to handle title of settings group

--- app/utils/conf.py
+++ app/utils/conf.py
@@ -1,12 +1,14 @@
 from os import path
 import tomllib
 
-def read_conf(filename: str = 'conf.toml') -> dict:
+def read_conf(conf_title: str, filename: str = 'conf.toml') -> dict:
     """
-    Function take filename to config toml file in project's root directory as a single argument,
+    Function takes filename to config toml file in project's root directory as a first argument,
+    name of the settings heading from toml file, 
     returns config data as a dictionary.
     """
     conf_path = path.join(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))), filename)
     with open(conf_path, 'rb') as f:
-        conf = tomllib.load(f)
+        data = tomllib.load(f)
+        conf = data.get(conf_title)
         return conf
