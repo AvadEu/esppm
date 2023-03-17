6eee064a55f2d7fa743adced468d5da1818022cd
AvadEu <dutwik44@wp.pl>
2023-03-17 14:44:42 +0100

    Create separate module for JWT abstraction

---
+++ app/security/jwt.py
@@ -0,0 +1,40 @@
+import jwt
+
+import random, string
+from datetime import datetime, timedelta
+
+class JWTEngine():
+    def __init__(
+            self, 
+            secret: str | None = None,
+            algorithm: str = "HS512", 
+            key_length: int = 64
+        ) -> None:
+        self.algorithm = algorithm
+        if secret:
+            self.secret = secret
+        self.key_length = key_length
+        self.secret = self.__generate_secret()
+
+
+    def __generate_secret(self) -> str:
+        secret_key = "".join(
+            random.choice(string.hexdigits) for _ in range(self.key_length)
+        )
+        return secret_key
+
+
+    def encode(self, data: dict, expires_delta: timedelta | None = None) -> str:
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=10)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, self.secret, algorithm=self.algorithm)
+        return encoded_jwt
+
+
+    def decode(self, to_decode: str) -> dict:
+        payload = jwt.decode(to_decode, self.secret, algorithms=[self.algorithm])
+        return payload
