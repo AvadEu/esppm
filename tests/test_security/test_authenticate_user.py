fb049b09cb135f8f3d714a7621d7d2636ac21708
AvadEu <dutwik44@wp.pl>
2023-03-21 16:29:20 +0100

    Create unittest for authenticate_user.py security module

---
+++ tests/test_security/test_authenticate_user.py
@@ -0,0 +1,21 @@
+from app.security.authenticate_user import authenticate_user
+from tests.conftest import user, sample_user
+
+from unittest import mock
+
+@mock.patch(
+        'app.security.authenticate_user.get_user_by_username',
+        return_value=sample_user,
+        autospec=True
+)
+def test_authenticate_user(mock_get_user_by_username, user):
+    assert authenticate_user("test_username", "test_password") == user
+
+
+@mock.patch(
+        'app.security.authenticate_user.get_user_by_username',
+        return_value=None,
+        autospec=True
+)
+def test_authenticate_user_not_exists(mock_get_user_by_username):
+    assert authenticate_user("not", "exists") == False
