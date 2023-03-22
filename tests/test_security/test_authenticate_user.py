51311cd174920e975ede3a7d56aa454d67b7904c
AvadEu <dutwik44@wp.pl>
2023-03-22 14:26:49 +0100

    Fix indentation

--- tests/test_security/test_authenticate_user.py
+++ tests/test_security/test_authenticate_user.py
@@ -5,18 +5,18 @@ from unittest import mock
 
 
 @mock.patch(
-        'app.security.authenticate_user.get_user_by_username',
-        return_value=sample_user,
-        autospec=True
+    'app.security.authenticate_user.get_user_by_username',
+    return_value=sample_user,
+    autospec=True
 )
 def test_authenticate_user(mock_get_user_by_username, user):
     assert authenticate_user("test_username", "test_password") == user
 
 
 @mock.patch(
-        'app.security.authenticate_user.get_user_by_username',
-        return_value=None,
-        autospec=True
+    'app.security.authenticate_user.get_user_by_username',
+    return_value=None,
+    autospec=True
 )
 def test_authenticate_user_not_exists(mock_get_user_by_username):
     assert authenticate_user("not", "exists") is False
