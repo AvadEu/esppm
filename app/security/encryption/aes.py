653a3bbd82053728a39ec45ef4e629b48bfa58a0
AvadEu <dutwik44@wp.pl>
2023-03-24 13:09:31 +0100

    Create abstract function for encrypting and decrypting records

--- app/security/encryption/aes.py
+++ app/security/encryption/aes.py
@@ -1,7 +1,10 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
+from app.api.models.domain.records import Record
+from app.api.models.schemas.records import RecordPydantic
 
-def encrypt(to_encrypt: str, key: bytes, initialization_vector: bytes) -> bytes:
+
+def __encrypt(to_encrypt: str, key: bytes, initialization_vector: bytes) -> bytes:
     to_encrypt = to_encrypt.encode()
     encryptor = Cipher(
         algorithm=algorithms.AES256(key=key),
@@ -11,10 +14,40 @@ def encrypt(to_encrypt: str, key: bytes, initialization_vector: bytes) -> bytes:
     return ciphertext
 
 
-def decrypt(ciphertext: bytes, key: bytes, initialization_vector: bytes) -> str:
+def __decrypt(ciphertext: bytes, key: bytes, initialization_vector: bytes) -> str:
     decryptor = Cipher(
         algorithm=algorithms.AES256(key=key),
         mode=modes.CFB(initialization_vector)
     ).decryptor()
     plain = decryptor.update(ciphertext) + decryptor.finalize()
     return plain.decode()
+
+
+def encrypt_record(
+    username: str,
+    record: RecordPydantic,
+    key: bytes,
+    initialization_vector: bytes
+            ) -> Record:
+    encrypted_record = Record(
+        service=record.service,
+        login=__encrypt(record.login, key, initialization_vector),
+        password=__encrypt(record.password, key, initialization_vector),
+        owner=username,
+        iv=initialization_vector
+    )
+    return encrypted_record
+
+
+def decrypt_record(
+    record: Record,
+    key: bytes,
+    initialization_vector: bytes
+        ) -> dict:
+    decrypted_record = {
+        "record_id": record.id,
+        "service": record.service,
+        "login": __decrypt(record.login, key, initialization_vector),
+        "password": __decrypt(record.password, key, initialization_vector)
+    }
+    return decrypted_record
--- tests/test_security/test_encryption/test_aes.py
+++ tests/test_security/test_encryption/test_aes.py
@@ -1,9 +1,17 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import pytest
 
-from app.security.encryption.aes import encrypt, decrypt
+from app.security.encryption.aes import (
+    __encrypt,
+    __decrypt,
+    encrypt_record,
+    decrypt_record
+)
+from app.api.models.domain.records import Record
+from app.api.models.schemas.records import RecordPydantic
 
 import os
+from unittest import mock
 
 TO_ENCRYPT = "Secret message!"
 KEY = os.urandom(32)
@@ -21,16 +29,59 @@ def encrypted_payload() -> bytes:
 
 
 def test_encrypt(encrypted_payload) -> None:
-    encrypted = encrypt(TO_ENCRYPT, KEY, IV)
+    encrypted = __encrypt(TO_ENCRYPT, KEY, IV)
     assert isinstance(encrypted, bytes)
     assert encrypted == encrypted_payload
 
 
 def test_decrypt(encrypted_payload) -> None:
-    decrypted = decrypt(
+    decrypted = __decrypt(
         ciphertext=encrypted_payload,
         key=KEY,
         initialization_vector=IV
         )
     assert isinstance(decrypted, str)
     assert decrypted == TO_ENCRYPT
+
+
+def test_encrypt_record(record_response_model) -> None:
+    sample_record = RecordPydantic(
+        service=record_response_model["service"],
+        login=record_response_model["login"],
+        password=record_response_model["password"]
+    )
+    encrypted_record = encrypt_record(
+        username="test_owner",
+        record=sample_record,
+        key=KEY,
+        initialization_vector=IV
+    )
+    decrypted_sample = decrypt_record(encrypted_record, KEY, IV)
+    decrypted_sample["record_id"] = 404
+    assert isinstance(encrypted_record, Record)
+    assert decrypted_sample == record_response_model
+
+
+@mock.patch(
+    target="app.security.encryption.aes.__decrypt",
+    return_value="decrypted_test_data",
+    autospec=True
+)
+def test_decrypt_record(
+    mock_decrypt_record,
+    record_response_model
+        ) -> None:
+    sample_record = Record(
+        service="test_service1",
+        login=b"decrypted_test_data",
+        password=b"decrypted_test_data",
+        owner="test_owner",
+        iv=b"iv"
+
+    )
+    sample_record.id = 404
+    decrypted_record = decrypt_record(
+        record=sample_record,
+        key=b"SECRET",
+        initialization_vector=b"IV")
+    assert decrypted_record == record_response_model
