daa2ff1dd119f95291c572279fbae6513b7db786
AvadEu <dutwik44@wp.pl>
2023-03-25 14:59:08 +0100

    Create separate dependency for recrod update

---
+++ app/api/dependencies/records.py
@@ -0,0 +1,36 @@
+from app.api.models.domain.records import Record
+from app.api.models.schemas.records import RecordPydantic, RecordUpdatePydantic
+from app.security.encryption.aes import encrypt_record, decrypt_record
+from app.api.db.services import update_record_in_db
+
+
+def update_record_data(
+    username: str,
+    key: bytes,
+    old_record: Record,
+    updated_record_body: RecordUpdatePydantic
+        ) -> None:
+    old_decrypted_record = decrypt_record(
+        record=old_record,
+        key=key,
+        initialization_vector=old_record.iv
+    )
+    record_body_dict = {
+        k: v for k, v in updated_record_body.dict().items() if v is not None
+        }
+    new_record_body = RecordPydantic(
+        service=record_body_dict.get("service", old_decrypted_record["service"]),
+        login=record_body_dict.get("login", old_decrypted_record["login"]),
+        password=record_body_dict.get("password", old_decrypted_record["password"])
+    )
+    new_record_encrypted = encrypt_record(
+        owner_username=username,
+        record=new_record_body,
+        key=key,
+        initialization_vector=old_record.iv
+    )
+    update_record_in_db(
+        old_decrypted=old_decrypted_record,
+        new_record=new_record_encrypted
+    )
+    return None
--- app/api/routes/records.py
+++ app/api/routes/records.py
@@ -12,12 +12,12 @@ from app.api.models.domain.records import Record
 from app.api.routes.authentication import get_current_user
 from app.security.encryption.aes import encrypt_record, decrypt_record
 from app.security.encryption.generate_cipher_key import generate_cipher_key
+from app.api.dependencies.records import update_record_data
 from app.api.db.services import (
     add_to_db,
     get_secret_by_owner,
     get_record_by_id,
     get_all_records_by_owner,
-    update_record_in_db,
     delete_obj_by_id
     )
 
@@ -100,26 +100,11 @@ def update_record(
             detail="There is no record of id {} in database"
             .format(record_id)
         )
-    old_decrypted_record = decrypt_record(
-        record=old_record,
+    update_record_data(
+        username=user.username,
         key=dec_key,
-        initialization_vector=old_record.iv
-    )
-    record_body_dict = {k: v for k, v in record_body.dict().items() if v is not None}
-    new_record_body = RecordPydantic(
-        service=record_body_dict.get("service", old_decrypted_record["service"]),
-        login=record_body_dict.get("login", old_decrypted_record["login"]),
-        password=record_body_dict.get("password", old_decrypted_record["password"])
-    )
-    new_record_encrypted = encrypt_record(
-        owner_username=user.username,
-        record=new_record_body,
-        key=dec_key,
-        initialization_vector=old_record.iv
-    )
-    update_record_in_db(
-        old_decrypted=old_decrypted_record,
-        new_record=new_record_encrypted
+        old_record=old_record,
+        updated_record_body=record_body
     )
     return JSONResponse(
         status_code=status.HTTP_200_OK,
