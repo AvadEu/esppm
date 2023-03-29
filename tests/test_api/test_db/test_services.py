3acc480b27107e8f0a40286615965b5191d10190
AvadEu <dutwik44@wp.pl>
2023-03-29 22:04:10 +0200

    Create unittest for get_user_by_username service

--- tests/test_api/test_db/test_services.py
+++ tests/test_api/test_db/test_services.py
@@ -1,4 +1,5 @@
 import pytest
+from sqlalchemy.exc import IntegrityError
 
 from app.api.db.DatabaseConnection import DatabaseConnection
 from app.utils.unittest_db import clean_test_database
@@ -28,24 +29,22 @@ def test_database_connection() -> DatabaseConnection:
     return DatabaseConnection("sqlite:///test.db")
 
 
-@pytest.fixture
-def manual_database_connection() -> sqlite3.Cursor:
-    con = sqlite3.connect("test.db")
-    cur = sqlite3.Cursor(con)
-    con.close()
-    return cur
+@pytest.fixture()
+def test_user_payload(user: User) -> dict:
+    payload = user.get_token_payload()
+    payload["password_hash"] = user.password_hash
+    return payload
 
 
 @mock.patch(target="app.api.db.services.db_connection.get_engine")
 def test_add_to_db(
     mock_db_get_engine: mock.MagicMock,
     test_database_connection: DatabaseConnection,
+    test_user_payload: dict,
     user: User
         ) -> None:
     mock_db_get_engine.return_value = test_database_connection.get_engine()
     user.created_at = datetime.now()
-    payload = user.get_token_payload()
-    payload["password_hash"] = user.password_hash
     add_to_db(user)
     con = sqlite3.connect("test.db")
     cur = sqlite3.Cursor(con)
@@ -58,7 +57,29 @@ def test_add_to_db(
         "last_name": user_data[3],
         "password_hash": user_data[4]
     }
-    assert payload.get("username") == new_user_dict.get("username")
-    assert payload.get("first_name") == new_user_dict.get("first_name")
-    assert payload.get("last_name") == new_user_dict.get("last_name")
-    assert payload.get("password_hash") == new_user_dict.get("password_hash")
+    assert test_user_payload.get("username") == new_user_dict.get("username")
+    assert test_user_payload.get("first_name") == new_user_dict.get("first_name")
+    assert test_user_payload.get("last_name") == new_user_dict.get("last_name")
+    assert test_user_payload.get("password_hash") == new_user_dict.get("password_hash")
+
+
+@mock.patch(target="app.api.db.services.db_connection.get_engine")
+def test_get_user_by_username(
+    mock_db_get_engine: mock.MagicMock,
+    test_database_connection: DatabaseConnection,
+    test_user_payload: dict,
+    user: User
+        ) -> None:
+    mock_db_get_engine.return_value = test_database_connection.get_engine()
+    username = test_user_payload.get("username")
+    try:
+        user.created_at = datetime.now()
+        add_to_db(user)
+    except IntegrityError:
+        assert False
+    user_from_db = get_user_by_username(username)
+    print(user_from_db, username)
+    assert test_user_payload.get("username") == user_from_db.username
+    assert test_user_payload.get("first_name") == user_from_db.first_name
+    assert test_user_payload.get("last_name") == user_from_db.last_name
+    assert test_user_payload.get("password_hash") == user_from_db.password_hash
